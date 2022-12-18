05e6a7d0e3b76154459f4bf59e913eb7e2d7131b
fanksli <1059098842@qq.com>
2022-12-18 22:01:39 +0800

    多模块的实现

--- src/__tests__/bundle.spec.js
+++ src/__tests__/bundle.spec.js
@@ -6,10 +6,47 @@ describe('bundle Test', () => {
     test('fetchModule', () => {
         const bundle = new Bundle({ entry: './a.js'});
         fs.readFileSync.mockReturnValueOnce(`const a = 1;`);
-        bundle.fetchModule('index.js');
+       const module = bundle.fetchModule('index.js');
         const {calls} = fs.readFileSync.mock;
-        
-        console.log(calls);
         expect(calls[0][0]).toEqual('index.js');
+        expect(module.code.toString()).toEqual('const a = 1;');
+    });
+
+    describe('build', () => {
+        test('单条语句', () => {
+            const bundle = new Bundle({ entry: './a.js'});
+            fs.readFileSync.mockReturnValueOnce(`console.log(1);`);
+            bundle.build('bundle.js');
+            const {calls} = fs.writeFileSync.mock;
+            
+            expect(calls[0][0]).toEqual('bundle.js');
+            expect(calls[0][1]).toEqual('console.log(1);');
+        });
+
+        test('多条语句', () => {
+            const bundle = new Bundle({ entry: './a.js'});
+            fs.readFileSync.mockReturnValueOnce(`const a = () => 1;
+            const b = () => 2;
+            a();`);
+            fs.writeFileSync.mock.calls = [];
+            bundle.build('bundle.js');
+            const {calls} = fs.writeFileSync.mock;
+            
+            expect(calls[0][0]).toEqual('bundle.js');
+            expect(calls[0][1]).toEqual(`const a = () => 1;
+a();`);
+        });
+        test('多模块语句', () => {
+            const bundle = new Bundle({ entry: 'index.js'});
+            fs.readFileSync.mockReturnValueOnce(`import {a} from './a';
+a();`).mockReturnValueOnce('export const a = () => 1;');
+            fs.writeFileSync.mock.calls = [];
+            bundle.build('bundle.js');
+            const {calls} = fs.writeFileSync.mock;
+            
+            expect(calls[0][0]).toEqual('bundle.js');
+            expect(calls[0][1]).toEqual(`const a = () => 1;
+a();`);
+        });
     });
 });
--- src/bundle.js
+++ src/bundle.js
@@ -5,7 +5,7 @@ const MagicString = require('magic-string');
 
 class Bundle {
     constructor({ entry }) {
-        this.entryPath = entry.replace(/\.js$/, '') + '.js';
+        this.entryPath = entry;
         this.modules = [];
     }
 
@@ -17,9 +17,9 @@ class Bundle {
     fetchModule(importee, importer) {
         let router;
 
-        if(importer) {
+        if(!importer) {
             // 主模块
-            router = importer;
+            router = importee;
         } else {
 
             if(path.isAbsolute(importee)) {
@@ -44,6 +44,35 @@ class Bundle {
 
             return module;
         }
+    }
+
+    build(outputFileName) {
+        const module = this.fetchModule(this.entryPath);
+
+       this.statements = module.expandAllStatement();
+
+        const {code} = this.generate();
+        fs.writeFileSync(outputFileName, code, 'utf-8');
+    }
+
+    generate() {
+        const magicString = new MagicString.Bundle();
+
+        this.statements.forEach(statement => {
+            const source = statement._source.clone();
+
+            if(statement.type === 'ExportNamedDeclaration') {
+                source.remove(statement.start, statement.declaration.start);
+            }
+
+            magicString.addSource({
+                content: source,
+                separtor: '\n',
+            });
+
+        })
+
+        return {code: magicString.toString()}
     }
 }
 
